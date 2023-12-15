877b672917eb33454894e1b14afd8890131175b5
sangjunnlee <134983124+sangjunnlee@users.noreply.github.com>
2023-12-15 14:52:04 +0000

    Feat: main function created

--- leetcode-tracker.py
+++ leetcode-tracker.py
@@ -42,4 +42,28 @@ def get_leetcode_info_by_id(id):
         sys.exit(errt)
     except RequestException as err:
         sys.exit(err)
-        
+
+def update_page(page_id: str, data:dict):
+    url = f"https://api.notion.com/v1/pages/{page_id}"
+
+    payload = {"properties": data}
+
+    res = requests.patch(url, json=payload, headers=headers)
+    return res
+
+def main():
+    parser = argparse.ArgumentParser(
+        description = "Generate leetcode question to fill up Notion for tracking of questions")
+    parser.add_argument("leetcode_number", type=int, help="leetcode question number")
+    parser.add_argument(
+        "comment", nargs="?", default="", help="optional comment for the question"
+    )
+    args = parser.parse_args()
+
+    leetcode_number_input_by_user = args.leetcode_number
+    leetcode_comment_input_by_user = args.comment
+
+    leet_code = get_leetcode_info_by_id(leetcode_number_input_by_user)
+
+    print(leet_code)
+
