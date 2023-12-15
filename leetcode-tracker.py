4eaddb18cf256c1bfcc703729a04239af0674192
sangjunnlee <134983124+sangjunnlee@users.noreply.github.com>
2023-12-15 14:39:38 +0000

    Feat: leetcode-tracker get-leetcode info

--- leetcode-tracker.py
+++ leetcode-tracker.py
@@ -1,5 +1,45 @@
 import requests
+import json
+import argparse
+from requests.exceptions import HTTPError, ConnectionError, Timeout, RequestException
+import sys
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+
 NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+DATABASE_ID = os.getenv("DATABASE_ID")
+
+NOTION_BASE_URL = "https://api.notion.com/"
+LEETCODE_INFO_URL = "https://lcid.cc/info/"
+LEETCODE_URL = "https://leetcode.com/problems/"
+
+urlDatabase = f"{NOTION_BASE_URL}v1/databases/{DATABASE_ID}"
+urlQueryPages = f"{NOTION_BASE_URL}v1/database/{DATABASE_ID}/query"
+
+headers = {
+    "Authorization": "Bearer " + NOTION_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28",
+}
+
+def get_leetcode_info_by_id(id):
+    try:
+        leetcode_url = f"{LEETCODE_INFO_URL}{id}"
+        resInfo = requests.get(leetcode_url, timeout=3)
+        resInfo = resInfo.json()
+        if "code" in resInfo:
+            if resInfo["code"] != 200:
+                raise RequestException(resInfo["message"])
+            resInfo.raise_for_status()
+        return resInfo
+    except HTTPError as errh:
+        sys.exit(errh)
+    except ConnectionError as errc:
+        sys.exit(errc)
+    except Timeout as errt:
+        sys.exit(errt)
+    except RequestException as err:
+        sys.exit(err)
+        
